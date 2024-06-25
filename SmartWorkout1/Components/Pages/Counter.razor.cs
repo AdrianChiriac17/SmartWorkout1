307199e168adecb8997a18245d26fc99c9788b21
Adrian <chiriac.adrian.q7w@student.ucv.ro>
2024-06-25 11:57:51 +0300

    sesiunea 1

---
+++ SmartWorkout1/Components/Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace SmartWorkout1.Components.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+
+        bool alertvisible = true;
+
+        string name;
+
+}
+}
---
+++ SmartWorkout1/Components/ShowName.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SmartWorkout1.Components
+{
+    public partial class ShowName : ComponentBase
+    {
+        [Parameter]
+        public string input { get; set; }
+    }
+}
